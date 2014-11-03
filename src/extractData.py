14e931366d7b44ce860bbd196e10656e65e3b4a8
David Qu <davidqu12345@gmail.com>
2014-11-02 19:09:49 -0800

    Added processing players, byes, and injuries to extractData script

--- src/extractData.py
+++ src/extractData.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 import os
-os.system('wget http://www.fantasyfootballnerd.com/service/nfl-teams/json/pyxyugixjvn4/ -O ../json/nfl-teams.json')
+url = 'http://www.fantasyfootballnerd.com/service/'
+ffn_id = 'json/pyxyugixjvn4/'
+services = ['players', 'byes', 'injuries']
+json_dir = '../json/'
+
+def extract_from(service, ext):
+    return
+
+for s in services:
+    os.system('wget ' + url + s + '/' + ffn_id + ' -O' + ' ' + json_dir + s + '.json') 
+os.system('wget ' + url + 'nfl-teams/json/pyxyugixjvn4/ -O ../json/nfl-teams.json')
